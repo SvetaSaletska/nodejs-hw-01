f6efb37bdb22d029e022bd640251551c77dcfb9e
SvetaSaletska <svitlanagalevich@gmail.com>
2024-07-12 00:40:29 +0300

    changes

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,11 @@
-export const PATH_DB =
+// import * as fs from 'node:fs/promises';
+import path from 'path';
+
+// function read() {
+//   const PATH_DB = path.resolve('db', 'db.json');
+//   return fs.readFile(PATH_DB, { encoding: 'UTF-8' });
+// }
+
+// export default { read };
+
+export const PATH_DB = path.resolve('db', 'db.json');
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,4 +1,10 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+fs.writeFile('db.json', JSON.stringify(createFakeContact, undefined, 2))
+  .then(console.log)
+  .catch(console.error);
 
 const generateContacts = async (number) => {};
 
