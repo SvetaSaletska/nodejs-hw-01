5e91dd2346c3e1d0e2974adf05d4fab93d187681
SvetaSaletska <svitlanagalevich@gmail.com>
2024-07-28 17:24:56 +0300

    corrections

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -2,10 +2,22 @@ import { PATH_DB } from '../constants/contacts.js';
 import fs from 'fs/promises';
 import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async () => {
+const generateContacts = async (number) => {
   try {
-    const data = createFakeContact();
-    await fs.writeFile(PATH_DB, JSON.stringify(data, undefined, 2), 'utf-8');
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+
+    const contacts = JSON.parse(data);
+
+    for (let i = 1; i <= number; i += 1) {
+      const newContacts = createFakeContact();
+      contacts.push(newContacts);
+    }
+
+    await fs.writeFile(
+      PATH_DB,
+      JSON.stringify(contacts, undefined, 2),
+      'utf-8',
+    );
   } catch (error) {
     console.error(error);
   }
